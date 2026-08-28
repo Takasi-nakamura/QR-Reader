0fcea866f07eeeec762a13d8a36046fef5473862
Takasi-nakamura <haruto.yomogida@gmail.com>
2026-08-28 21:35:20 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,74 @@
+const CACHE_NAME = "qr-reader-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+
+// Install
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+
+  );
+
+  self.skipWaiting();
+
+});
+
+
+// Activate
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+
+});
+
+
+// Fetch
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(cachedResponse => {
+
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(event.request);
+
+      })
+
+  );
+
+});
